a775bd88e0e9c0562e896b32c304c1dfeac0ea95
Danil Kozlov <megacokes@gmail.com>
2020-04-23 19:56:41 +0500

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -4,55 +4,7 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    caps: [
-      {
-        image: "777.jpg",
-        article: "0777",
-        description: "descr777",
-        tags: "tag1, tag2, tag3",
-        condition: "состояние",
-        country: "Россия",
-        pivzavod: "pivzavod777"
-      },
-      {
-        image: "777.jpg",
-        article: "0777",
-        description: "descr777",
-        tags: "tag1, tag2, tag3",
-        condition: "состояние",
-        country: "Россия",
-        pivzavod: "pivzavod777"
-      },
-      {
-        image: "777.jpg",
-        article: "0777",
-        description: "descr777",
-        tags: "tag1, tag2, tag3",
-        condition: "состояние",
-        country: "Россия",
-        pivzavod: "pivzavod777"
-      },
-      {
-        image: "777.jpg",
-        article: "0777",
-        description: "descr777",
-        tags: "tag1, tag2, tag3",
-        condition: "состояние",
-        country: "Россия",
-        pivzavod: "pivzavod777"
-      },
-      {
-        image: "777.jpg",
-        article: "0777",
-        description: "descr777",
-        tags: "tag1, tag2, tag3",
-        condition: "состояние",
-        country: "Россия",
-        pivzavod: "pivzavod777"
-      }
-    ]
-  },
+  state: {},
 
   getters: {},
 
