92bc2a0f366983e645ba9180442488aa5f778c72
Danil Kozlov <megacokes@gmail.com>
2020-08-17 13:34:42 +0500

    Add vue-js-modal

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from "./App.vue";
 import store from "./store/index.js";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
+import VModal from "vue-js-modal";
 
 Vue.config.productionTip = false;
+
+Vue.use(VModal);
 
 new Vue({
   store,
