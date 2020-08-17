80ad21ab1b890f14cfc1765c9f7c0708e3591db0
Danil Kozlov <megacokes@gmail.com>
2020-08-17 23:56:05 +0500

    Beautiful

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,26 +1,26 @@
 <template lang="pug">
-    footer.footer.bg-secondary.fixed-bottom
-      ul
-        li
-          a.text-white(href='https://github.com/nklnke/crowncaps-collection' target='_blank') github
-        li |
-        li
-          a.text-white(href='https://vuejs.org' target='_blank') vue.js
-        li
-          a.text-white(href='https://cli.vuejs.org/' target='_blank') vue-cli
-        li
-          a.text-white(href='https://vuex.vuejs.org' target='_blank') vuex
-        li
-          a.text-white(href='https://getbootstrap.com' target='_blank') bootstrap
-        li |
-        li
-          a.text-white(href='https://nodejs.org' target='_blank') node.js
-        li
-          a.text-white(href='https://github.com/typicode/json-server' target='_blank') json-server
-        li
-          #btnTop.fixed-action-btn.smooth-scroll(style='bottom: 45px; right: 24px; display: none;')
-            a.btn-floating.btn-large.red(href='#topSection')
-              i.fa.fa-arrow-up
+  footer.footer.bg-secondary.fixed-bottom
+    ul
+      li
+        a.text-white(href='https://github.com/nklnke/crowncaps-collection' target='_blank') github
+      li |
+      li
+        a.text-white(href='https://vuejs.org' target='_blank') vue.js
+      li
+        a.text-white(href='https://cli.vuejs.org/' target='_blank') vue-cli
+      li
+        a.text-white(href='https://vuex.vuejs.org' target='_blank') vuex
+      li
+        a.text-white(href='https://getbootstrap.com' target='_blank') bootstrap
+      li |
+      li
+        a.text-white(href='https://nodejs.org' target='_blank') node.js
+      li
+        a.text-white(href='https://github.com/typicode/json-server' target='_blank') json-server
+      li
+        #btnTop.fixed-action-btn.smooth-scroll(style='bottom: 45px; right: 24px; display: none;')
+          a.btn-floating.btn-large.red(href='#topSection')
+            i.fa.fa-arrow-up
 </template>
 
 <script>
